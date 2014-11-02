ba8bb04e3138530ec3e606f606311e7c77db9a50
vagrant <vagrant@precise64.(none)>
2014-11-02 06:07:26 +0000

    firsty

---
+++ hunter/wsgi.py
@@ -0,0 +1,14 @@
+"""
+WSGI config for hunter project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
+"""
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hunter.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
