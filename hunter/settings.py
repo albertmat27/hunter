164a83811b6aab25ef9afc04564ad3fdb95b0e38
vagrant <vagrant@precise64.(none)>
2014-11-02 06:23:43 +0000

    hello world

--- hunter/settings.py
+++ hunter/settings.py
@@ -10,6 +10,8 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import dj_database_url
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
@@ -57,10 +59,7 @@ WSGI_APPLICATION = 'hunter.wsgi.application'
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config()
 }
 
 # Internationalization
@@ -80,4 +79,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+MEDIA_ROOT = 'uploads'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "assets"),
+)
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, "templates"),
+)
--- hunter/urls.py
+++ hunter/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
@@ -5,6 +6,13 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'hunter.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^$', 'hunter.views.home', name='home'),
 
     url(r'^admin/', include(admin.site.urls)),
+)
+
+urlpatterns += patterns('',
+    (r'^static/(.*)$', 'django.views.static.serve', {
+        'document_root': settings.STATIC_ROOT
+        }),
 )
